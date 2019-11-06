1066d7af40d8ed490c6a23139ce55cc94af96441
akshatsoni64 <akshatsoni64@gmail.com>
2019-11-07 00:04:29 +0530

    environment updated

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/media/monster/634F8F527865B73D/PYTHON/DjangoMySQLDemo/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
